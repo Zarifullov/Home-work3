b67f916a9f7fe9cdb312b3c0878a32e257978b79
Renat <rzarifullov@gmail.com>
2022-07-26 19:05:03 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿int number = 0;
+
+Console.WriteLine("Введите пятизначное число: ");
+number = int.Parse(Console.ReadLine());
+
+String user = number;
+
+
+for(int i = 0; i < user.Length; i++)
+
+
+if(i == (user.Length-i))
+{
+    Console.WriteLine($"{number}-> Да");
+}
+else
+Console.WriteLine($"{number}-> Нет");
