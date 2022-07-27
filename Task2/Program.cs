950c00e347465734b24fdc73a885e503128013a8
Renat <rzarifullov@gmail.com>
2022-07-27 20:41:51 +0300

    Task2 added

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,38 +1,25 @@
-﻿/*
-int GetCountNum (int a)
+﻿int x1 = Coordinate("x", "A");
+int y1 = Coordinate("y", "A");
+int z1 = Coordinate("z", "A");
+int x2 = Coordinate("x", "B");
+int y2 = Coordinate("y", "B");
+int z2 = Coordinate("z", "B");
+
+int Coordinate(string coorName, string pointName)
 {
-int count = 0;
-for(int i = 1; i <= a; i++)
-{
-count = 1 + i;
-
-}
-return count;
-}
-
-int A = int.Parse(Console.ReadLine());
-Console.Write($"Количество цифр в числе {A} = {GetCountNum (A)}");
-
-Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
-
-4  -> 24
-5  -> 120
-*/
-
-int n = 0;
-
-Console.WriteLine("Введите число N: ");
-n = int.Parse(Console.ReadLine());
-
-int GetNum (int n)
-{
-int count = 1;
-
-for(int i = 1; i <= n; i++)
-{
-count = count * i;
-}
-return count;
+    Console.Write($"Введите координату {coorName} точки {pointName}: ");
+    return Convert.ToInt16(Console.ReadLine());
 }
 
-Console.Write($"Произведение чисел от 1 до {n} = {GetNum (n)}");
+double Decision(double x1, double x2, 
+                double y1, double y2, 
+                double z1, double z2)
+{
+  return Math.Sqrt(Math.Pow((x2-x1), 2) + 
+                   Math.Pow((y2-y1), 2) + 
+                   Math.Pow((z2-z1), 2));
+}
+
+double segmentLength =  Math.Round (Decision(x1, x2, y1, y2, z1, z2), 2 );
+
+Console.WriteLine($"Длина отрезка  {segmentLength}");
