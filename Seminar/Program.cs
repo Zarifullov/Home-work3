bd0e75546e4568286730455e401ba2088e6efdd9
Renat <rzarifullov@gmail.com>
2022-07-29 08:14:10 +0300

    Task3 added

---
+++ Seminar/Program.cs
@@ -0,0 +1,15 @@
+﻿// создать массив и заполнить его от 1 до размера массива N
+
+int n = int.Parse(Console.ReadLine());
+int[] array = new int [n];
+
+
+for (int i = 0; i < n; i++)
+{
+   array[i] = i + 1;
+Console.Write($"{array[i]} ");
+}
+//foreach (int element in array);
+
+
+
---
+++ Task3/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+
+Console.Write("Введите число: ");
+int cube = int.Parse(Console.ReadLine());
+
+void Cube(int[] cube)
+{
+  int counter = 0;
+  int length = cube.Length;
+  while (counter <  length){
+    cube[counter] = counter*counter*counter;
+    counter++;
+  }
+}
+
+void PrintArry(int[] collection)
+{
+  int count = collection.Length;
+  int index = 0;
+  while(index < count){
+    Console.Write(collection[index]+ " ");
+    index++;
+  }
+} 
+
+int[] array = new int[cube+1];
+Cube(array);
+PrintArry(array);
